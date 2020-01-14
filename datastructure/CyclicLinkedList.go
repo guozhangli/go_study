47d27a4cc96cc190e49d08bb826d60037c7de48a
gzli <364627306@qq.com>
2020-01-14 23:42:23 +0800

    develop cycliclinkedlist

--- datastructure/CyclicLinkedList.go
+++ datastructure/CyclicLinkedList.go
@@ -1,1 +1,100 @@
 package TestProject
+
+import "fmt"
+
+type CyclicLinkedList struct {
+	data     interface{}
+	nextNode *CyclicLinkedList
+}
+
+func NewCyclicLinkedList(value interface{}) *CyclicLinkedList {
+	cycLinkedList := &CyclicLinkedList{
+		data:     value,
+		nextNode: nil,
+	}
+	return cycLinkedList
+}
+
+func checkCyclicLinkedList(list *CyclicLinkedList) {
+	if list == nil {
+		panic("未创建循环链表")
+	}
+}
+
+func (list *CyclicLinkedList) HeadAdd(value interface{}) {
+	checkCyclicLinkedList(list)
+	headNode := list
+	node := NewCyclicLinkedList(value)
+	if headNode.nextNode != nil {
+		node.nextNode = headNode.nextNode
+		headNode.nextNode = node
+	} else {
+		headNode.nextNode = node
+		node.nextNode = headNode
+	}
+}
+
+func (list *CyclicLinkedList) TailAdd(value interface{}) {
+	checkCyclicLinkedList(list)
+	headNode := list
+	node := NewCyclicLinkedList(value)
+	var currNode = headNode
+	for currNode != nil && currNode.nextNode != headNode {
+		currNode = currNode.nextNode
+	}
+	if currNode == nil {
+		headNode.nextNode = node
+		node.nextNode = headNode
+	} else {
+		currNode.nextNode = node
+		node.nextNode = headNode
+	}
+}
+
+func (list *CyclicLinkedList) Print(head *CyclicLinkedList) {
+	cycLinkedList := list
+	for cycLinkedList != nil && cycLinkedList.nextNode != head {
+		fmt.Println(cycLinkedList)
+		cycLinkedList = cycLinkedList.nextNode
+	}
+	fmt.Println(cycLinkedList)
+}
+
+func (list *CyclicLinkedList) insert(index int, value interface{}) bool {
+	head := list
+	cycLinkedList := list
+	node := NewCyclicLinkedList(value)
+	if index < 0 || index > list.length() {
+		return false
+	}
+	var count int
+	var currNode *CyclicLinkedList
+	for cycLinkedList != nil && cycLinkedList.nextNode != head {
+		if count == index {
+			currNode = cycLinkedList
+			break
+		}
+		cycLinkedList = cycLinkedList.nextNode
+		count++
+	}
+	if currNode != nil && currNode != head {
+		node.nextNode = currNode.nextNode
+		currNode.nextNode = node
+	} else {
+		node.nextNode = head.nextNode
+		head.nextNode = node
+
+	}
+	return true
+}
+
+func (list *CyclicLinkedList) length() int {
+	cycLinkedList := list
+	head := list
+	var count int
+	for cycLinkedList != nil && cycLinkedList.nextNode != head {
+		cycLinkedList = cycLinkedList.nextNode
+		count++
+	}
+	return count
+}
--- datastructure/CyclicLinkedList_test.go
+++ datastructure/CyclicLinkedList_test.go
@@ -1,1 +1,54 @@
 package TestProject
+
+import "testing"
+
+func TestCycLinkedList(t *testing.T) {
+	cycLinkedList := NewCyclicLinkedList(nil)
+	t.Log(cycLinkedList)
+}
+
+func initCycLinkedList() *CyclicLinkedList {
+	cycLinkedList := NewCyclicLinkedList(nil)
+	return cycLinkedList
+}
+
+func TestCycLinkedListHeadAdd(t *testing.T) {
+	cycLinkedList := initCycLinkedList()
+	head := cycLinkedList
+	cycLinkedList.HeadAdd("1111")
+	cycLinkedList.HeadAdd("2222")
+	cycLinkedList.HeadAdd("3333")
+	cycLinkedList.HeadAdd("4444")
+	cycLinkedList.Print(head)
+}
+
+func TestCycLinkedListTailAdd(t *testing.T) {
+	cycLinkedList, head := initCycLinkedListAddValue()
+	cycLinkedList.Print(head)
+}
+
+func TestCycLinkedListInsert(t *testing.T) {
+	cycLinkedList, head := initCycLinkedListAddValue()
+	if cycLinkedList.insert(4, "5555") {
+		cycLinkedList.Print(head)
+	} else {
+		t.Log("insert failed")
+	}
+
+}
+
+func initCycLinkedListAddValue() (*CyclicLinkedList, *CyclicLinkedList) {
+	cycLinkedList := initCycLinkedList()
+	head := cycLinkedList
+	cycLinkedList.TailAdd("1111")
+	cycLinkedList.TailAdd("2222")
+	cycLinkedList.TailAdd("3333")
+	cycLinkedList.TailAdd("4444")
+	return cycLinkedList, head
+}
+
+func TestCycLinkedListLength(t *testing.T) {
+	cycLinkedList, _ := initCycLinkedListAddValue()
+	len := cycLinkedList.length()
+	t.Log(len)
+}
