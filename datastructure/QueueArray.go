53d666d91317bf606a3e154e830055eaed542901
ligz <ligzh@irisking.com>
2020-01-17 18:24:43 +0800

    add about queueArray

--- datastructure/QueueArray.go
+++ datastructure/QueueArray.go
@@ -1,1 +1,85 @@
 package TestProject
+
+const capactity = 10
+
+type QueueArray struct {
+	Front int
+	Rear  int
+	Array [capactity]interface{}
+}
+
+func NewQueueArray() Queue {
+	var arr [capactity]interface{}
+	queueArray := &QueueArray{
+		Front: 0,
+		Rear:  0,
+		Array: arr,
+	}
+	return queueArray
+}
+
+func checkQueueArray(queueArray *QueueArray) {
+	if queueArray == nil {
+		panic("no queueArray created")
+	}
+}
+
+func IsFull(queue *QueueArray) bool {
+	checkQueueArray(queue)
+	return (queue.Rear+1)%capactity == queue.Front
+}
+
+func (queue *QueueArray) DeQueue() interface{} {
+	checkQueueArray(queue)
+	if queue.IsEmpty() {
+		return nil
+	}
+	var top = queue.Array[queue.Front]
+	queue.Array[queue.Front] = nil
+	var front = queue.Front
+	front++
+	queue.Front = front % capactity
+	return top
+}
+
+func (queue *QueueArray) EnQueue(value interface{}) {
+	checkQueueArray(queue)
+	if !IsFull(queue) {
+		queue.Array[queue.Rear] = value
+		var rear = queue.Rear
+		rear++
+		queue.Rear = rear % capactity
+	} else {
+		panic("queueArray is full")
+	}
+}
+
+func (queue *QueueArray) IsEmpty() bool {
+	checkQueueArray(queue)
+	return queue.Front == queue.Rear
+}
+
+func (queue *QueueArray) Length() int {
+	checkQueueArray(queue)
+	return (queue.Rear - queue.Front + capactity) % capactity
+}
+
+func (queue *QueueArray) Clear() {
+	checkQueueArray(queue)
+	for !queue.IsEmpty() {
+		queue.DeQueue()
+	}
+}
+
+func (queue *QueueArray) GetFront() interface{} {
+	checkQueueArray(queue)
+	if queue.IsEmpty() {
+		return nil
+	}
+	return queue.Array[queue.Front]
+}
+
+func (queue *QueueArray) Distroy(q *Queue) {
+	checkQueueArray(queue)
+	*q = nil
+}
--- datastructure/QueueArray_test.go
+++ datastructure/QueueArray_test.go
@@ -1,1 +1,120 @@
 package TestProject
+
+import (
+	"testing"
+)
+
+func initQueueArray() Queue {
+	queueArray := NewQueueArray()
+	return queueArray
+}
+
+func TestCreateQueueArray(t *testing.T) {
+	queueArray := initQueueArray()
+	t.Log(queueArray)
+}
+
+func TestQueueArray_IsFull(t *testing.T) {
+	queueArray := initQueueArray()
+	if IsFull(queueArray.(*QueueArray)) {
+		t.Log("queueArray is full")
+	} else {
+		t.Log("queueArray is not full")
+	}
+}
+
+func TestQueueArray_Length(t *testing.T) {
+	queueArray := initQueueArray()
+	queueArray.EnQueue("1111")
+	queueArray.EnQueue("2222")
+	queueArray.EnQueue("3333")
+	queueArray.EnQueue("4444")
+	queueArray.EnQueue("5555")
+	queueArray.EnQueue("6666")
+	len := queueArray.Length()
+	t.Log(len)
+}
+
+func TestQueueArray_EnQueue(t *testing.T) {
+	queueArray := initQueueArray()
+	queueArray.EnQueue("1111")
+	queueArray.EnQueue("2222")
+	queueArray.EnQueue("3333")
+	queueArray.EnQueue("4444")
+	queueArray.EnQueue("5555")
+	queueArray.EnQueue("6666")
+	queueArray.EnQueue("7777")
+	queueArray.EnQueue("8888")
+	queueArray.EnQueue("9999")
+
+	t.Log(queueArray)
+}
+
+func TestQueueArray_DeQueue(t *testing.T) {
+	queueArray := initQueueArray()
+	queueArray.EnQueue("1111")
+	queueArray.EnQueue("2222")
+	queueArray.EnQueue("3333")
+	queueArray.EnQueue("4444")
+	queueArray.DeQueue()
+	queueArray.EnQueue("5555")
+	queueArray.DeQueue()
+	t.Log(queueArray)
+}
+
+func TestQueueArray_GetFront(t *testing.T) {
+	queueArray := initQueueArray()
+	queueArray.EnQueue("1111")
+	queueArray.EnQueue("2222")
+	queueArray.EnQueue("3333")
+	queueArray.EnQueue("4444")
+	val := queueArray.GetFront()
+	t.Log(queueArray)
+	t.Log(val)
+}
+
+func TestQueueArray_IsEmpty(t *testing.T) {
+	queueArray := initQueueArray()
+	if queueArray.IsEmpty() {
+		t.Log("queueArray is empty")
+	} else {
+		t.Log("queueArray is not empty")
+	}
+}
+
+func TestQueueArray_Clear(t *testing.T) {
+	queueArray := initQueueArray()
+	queueArray.EnQueue("1111")
+	queueArray.EnQueue("2222")
+	queueArray.EnQueue("3333")
+	queueArray.EnQueue("4444")
+	queueArray.Clear()
+	queueArray.EnQueue("3333")
+	queueArray.EnQueue("4444")
+	queueArray.EnQueue("3333")
+	queueArray.EnQueue("4444")
+	queueArray.EnQueue("3333")
+	queueArray.EnQueue("4444")
+	queueArray.EnQueue("3333")
+	queueArray.EnQueue("4444")
+	queueArray.EnQueue("3333")
+
+	if IsFull(queueArray.(*QueueArray)) {
+		t.Log("queueArray is full")
+	} else {
+		t.Log("queueArray is not full")
+	}
+	t.Log(queueArray)
+}
+
+func TestQueueArray_Distroy(t *testing.T) {
+	queueArray := initQueueArray()
+	queueArray.EnQueue("1111")
+	queueArray.EnQueue("2222")
+	queueArray.EnQueue("3333")
+	queueArray.EnQueue("4444")
+	queueArray.Clear()
+	t.Log(queueArray)
+	queueArray.Distroy(&queueArray)
+	t.Log(queueArray)
+}
