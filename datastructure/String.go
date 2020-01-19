7969d0c0ad0111cb794a7d23ff83707fc231c46b
ligz <ligzh@irisking.com>
2020-01-19 18:02:10 +0800

    kmp

--- datastructure/String.go
+++ datastructure/String.go
@@ -1,6 +1,8 @@
 package TestProject
 
-func BruteForceStringMatch(s string, t string) int {
+type MyString string
+
+func BruteForceStringMatch(s MyString, t MyString) int {
 	s_len := len(s)
 	t_len := len(t)
 	for i := 0; i <= s_len-t_len; i++ {
@@ -10,6 +12,42 @@ func BruteForceStringMatch(s string, t string) int {
 		}
 		if j == t_len {
 			return i
+		}
+	}
+	return -1
+}
+
+//KMP
+//abaabbabaab
+func BuildFArray(s MyString) []int {
+	F := make([]int, len(s))
+	F[0] = -1
+	for i := 1; i < len(s); i++ {
+		var j = F[i-1]
+		for s[j+1] != s[i] && j >= 0 {
+			j = F[j]
+		}
+		if s[j+1] == s[i] {
+			F[i] = j + 1
+		} else {
+			F[i] = -1
+		}
+	}
+	return F
+}
+
+func (s MyString) IndexKMP(b MyString) int {
+	var i, j = 0, -1
+	f := BuildFArray(b)
+	for ; i < len(s); i++ {
+		for j > -1 && s[i] != b[j+1] {
+			j = f[j]
+		}
+		if s[i] == b[j+1] {
+			j++
+		}
+		if j == len(b)-1 {
+			return i - len(b) + 1
 		}
 	}
 	return -1
--- datastructure/String_test.go
+++ datastructure/String_test.go
@@ -6,3 +6,17 @@ func TestString_pattern(t *testing.T) {
 	index := BruteForceStringMatch("goodgoogle", "google")
 	t.Log(index)
 }
+
+func TestString_pattern2(t *testing.T) {
+	var str MyString = "abaabbabaab"
+	var F []int
+	F = BuildFArray(str)
+	t.Log(F)
+}
+
+func TestMyString_IndexKMP(t *testing.T) {
+	var b MyString = "abaabaabbabaaabaabbabaab"
+	var str MyString = "abaabbabaab"
+	index := b.IndexKMP(str)
+	t.Log(index)
+}
