155c7c3005d4ae7ad20003bcde80708ef4754dbc
gzli <364627306@qq.com>
2020-02-29 22:36:29 +0800

    graph orthogonal list

--- datastructure/GraphOList.go
+++ datastructure/GraphOList.go
@@ -8,8 +8,8 @@ type LinkNode struct {
 type Arc struct {
 	TailIndex int
 	HeadIndex int
-	HeadLink  LinkNode
-	TailLink  LinkNode
+	HeadLink  *LinkNode
+	TailLink  *LinkNode
 }
 
 type VeriexONode struct {
@@ -36,4 +36,107 @@ func InitGraphOList(value ...interface{}) *GraphOList {
 		graphOL.Veriexs[i] = veriexONode
 	}
 	return graphOL
+}
+
+func CheckGraphOList(graphOList *GraphOList) {
+	if graphOList == nil {
+		panic("未创建图")
+	}
+	if graphOList.Veriexs == nil {
+		panic("未创建图的顶点")
+	}
+}
+
+func (graphOList *GraphOList) AddOutArcInGraphOList(ti int, hi int, tw int) {
+	CheckGraphOList(graphOList)
+	if ti < 0 || ti >= len(graphOList.Veriexs) || hi < 0 || hi >= len(graphOList.Veriexs) {
+		panic("顶点索引值不合法")
+	}
+	arc := &Arc{
+		TailIndex: ti,
+		HeadIndex: hi,
+		HeadLink: &LinkNode{
+			Link:   nil,
+			Weight: INFINITY,
+		},
+		TailLink: &LinkNode{
+			Link:   nil,
+			Weight: tw,
+		},
+	}
+	ver := graphOList.Veriexs[ti]
+	if ver.FirstOut != nil {
+		node := ver.FirstOut
+		for node.TailLink.Link != nil {
+			node = node.TailLink.Link
+		}
+		node.TailLink.Link = arc
+	} else {
+		ver.FirstOut = arc
+	}
+}
+
+func (graphOList *GraphOList) AddInArcInGraphOList(ti int, hi int, hw int) {
+	CheckGraphOList(graphOList)
+	if ti < 0 || ti >= len(graphOList.Veriexs) || hi < 0 || hi >= len(graphOList.Veriexs) {
+		panic("顶点索引值不合法")
+	}
+	ver := graphOList.Veriexs[ti]
+	hVer := graphOList.Veriexs[hi]
+	node := hVer.FirstOut
+	for node != nil && node.HeadIndex != ti {
+		node = node.TailLink.Link
+	}
+
+	if ver.FirstIn != nil {
+		tempNode := ver.FirstIn
+		for tempNode.HeadLink.Link != nil {
+			tempNode = tempNode.HeadLink.Link
+		}
+		tempNode.HeadLink.Link = node
+	} else {
+		ver.FirstIn = node
+	}
+}
+
+func (graphOList *GraphOList) GetVeriex(value interface{}) *VeriexONode {
+	CheckGraphOList(graphOList)
+	var veriex *VeriexONode
+	for _, v := range graphOList.Veriexs {
+		if v.Value == value {
+			veriex = v
+			break
+		}
+	}
+	return veriex
+}
+
+func (graphOList *GraphOList) GetOutDegreeInGraphOList(value interface{}) int {
+	CheckGraphOList(graphOList)
+	veriex := graphOList.GetVeriex(value)
+	if veriex == nil {
+		panic("顶点不存在")
+	}
+	arc := veriex.FirstOut
+	var count int
+	for arc != nil {
+		arc = arc.TailLink.Link
+		count++
+	}
+	return count
+}
+
+func (graphOList *GraphOList) GetInDegreeInGraphOList(value interface{}) int {
+	CheckGraphOList(graphOList)
+	veriex := graphOList.GetVeriex(value)
+	if veriex == nil {
+		panic("顶点不存在")
+	}
+	arc := veriex.FirstIn
+	var count int
+	for arc != nil {
+		arc = arc.HeadLink.Link
+		count++
+	}
+	return count
 }
--- datastructure/GraphOList_test.go
+++ datastructure/GraphOList_test.go
@@ -10,7 +10,59 @@ func TestInitGraphOList(t *testing.T) {
 	printGOL(graphOList, t)
 }
 
+func InitGOL() *GraphOList {
+	graphOList := InitGraphOList("V0", "V1", "V2", "V3")
+	return graphOList
+}
+
 func printGOL(graph *GraphOList, t *testing.T) {
 	str, _ := json.Marshal(graph)
 	t.Log(string(str))
+}
+
+func createGraphOListOutAct(graphOList *GraphOList) {
+	graphOList.AddOutArcInGraphOList(0, 3, 7)
+	graphOList.AddOutArcInGraphOList(1, 0, 3)
+	graphOList.AddOutArcInGraphOList(1, 2, 4)
+	graphOList.AddOutArcInGraphOList(2, 0, 6)
+	graphOList.AddOutArcInGraphOList(2, 1, 5)
+}
+
+func createGraphOListInAct(graphOList *GraphOList) {
+	graphOList.AddInArcInGraphOList(0, 1, 3)
+	graphOList.AddInArcInGraphOList(0, 2, 6)
+	graphOList.AddInArcInGraphOList(1, 2, 5)
+	graphOList.AddInArcInGraphOList(2, 1, 4)
+	graphOList.AddInArcInGraphOList(3, 0, 7)
+}
+
+func TestAddOutArcInGraphOList(t *testing.T) {
+	graphOList := InitGOL()
+	createGraphOListOutAct(graphOList)
+	printGOL(graphOList, t)
+}
+
+func TestAddInArcInGraphOList(t *testing.T) {
+	graphOList := InitGOL()
+	createGraphOListOutAct(graphOList)
+	createGraphOListInAct(graphOList)
+	printGOL(graphOList, t)
+}
+
+func TestGetOutDegreeInGraphOList(t *testing.T) {
+	graphOList := InitGOL()
+	createGraphOListOutAct(graphOList)
+	createGraphOListInAct(graphOList)
+	printGOL(graphOList, t)
+	id := graphOList.GetOutDegreeInGraphOList("V3")
+	t.Log(id)
+}
+
+func TestGetInDegreeInGraphOList(t *testing.T) {
+	graphOList := InitGOL()
+	createGraphOListOutAct(graphOList)
+	createGraphOListInAct(graphOList)
+	printGOL(graphOList, t)
+	id := graphOList.GetInDegreeInGraphOList("V3")
+	t.Log(id)
 }
