f1f18864f7e839c0c73de10ff8d0327190ff5a83
ligz <ligzh@irisking.com>
2020-03-05 11:00:50 +0800

    minispantree by kurskal algoirgth

--- datastructure/GraphEdgeArray.go
+++ datastructure/GraphEdgeArray.go
@@ -1,5 +1,7 @@
 package TestProject
 
+import "fmt"
+
 //边集数组表示图
 type EdgeType struct {
 	Begin  int
@@ -8,14 +10,65 @@ type EdgeType struct {
 }
 type GraphEdgeArray struct {
 	Veriexs []interface{}
-	Edges   []*Edge
+	Edges   []*EdgeType
 }
 
 func InitGraphEdgeArray(value ...interface{}) *GraphEdgeArray {
 	vNum := len(value)
 	graphEdgeArray := &GraphEdgeArray{
 		Veriexs: make([]interface{}, vNum),
-		Edges:   make([]*Edge, vNum*(vNum-1)/2),
+		Edges:   make([]*EdgeType, 0),
+	}
+	for i, v := range value {
+		graphEdgeArray.Veriexs[i] = v
 	}
 	return graphEdgeArray
+}
+func checkGraphEdgeArray(graphEdgeArray *GraphEdgeArray) {
+	if graphEdgeArray == nil {
+		panic("未创建图")
+	}
+}
+func (graphEdgeArray *GraphEdgeArray) AddEdgeInGraphEA(begin int, end int, weight int) {
+	checkGraphEdgeArray(graphEdgeArray)
+	edge := &EdgeType{
+		Begin:  begin,
+		End:    end,
+		Weight: weight,
+	}
+	graphEdgeArray.Edges = append(graphEdgeArray.Edges, edge)
+}
+
+//最小生成树-克鲁斯卡尔算法
+func (graphEdgeArray *GraphEdgeArray) MiniSpanTree_Kruskal() *GraphEdgeArray {
+	checkGraphEdgeArray(graphEdgeArray)
+	edges := graphEdgeArray.Edges
+	for i := 0; i < len(edges); i++ {
+		for j := i; j < len(edges); j++ {
+			if edges[i].Weight > edges[j].Weight {
+				edges[i], edges[j] = edges[j], edges[i]
+			}
+		}
+	}
+	verNum := len(graphEdgeArray.Veriexs)
+	parent := make([]int, verNum)
+	for i := 0; i < verNum; i++ {
+		parent[i] = 0
+	}
+	for i := 0; i < len(edges); i++ {
+		m := Find(parent, edges[i].Begin)
+		n := Find(parent, edges[i].End)
+		if m != n {
+			parent[m] = n
+			fmt.Println(edges[i])
+		}
+	}
+	return graphEdgeArray
+}
+
+func Find(parent []int, f int) int {
+	for parent[f] > 0 {
+		f = parent[f]
+	}
+	return f
 }
--- datastructure/GraphEdgeArray_test.go
+++ datastructure/GraphEdgeArray_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func InitGEA() *GraphEdgeArray {
-	graph := InitGraphEdgeArray("V0", "V1", "V2", "V3")
+	graph := InitGraphEdgeArray("V0", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8")
 	return graph
 }
 
@@ -17,4 +17,26 @@ func printGEA(graph *GraphEdgeArray) string {
 func TestInitGraphEdgeArray(t *testing.T) {
 	graphEdgeArray := InitGEA()
 	t.Log(printGEA(graphEdgeArray))
+}
+
+func TestAddEdgeInGraphEA(t *testing.T) {
+	graphEdgeArray := InitGEA()
+	graphEdgeArray.AddEdgeInGraphEA(0, 1, 10)
+	graphEdgeArray.AddEdgeInGraphEA(0, 5, 11)
+	graphEdgeArray.AddEdgeInGraphEA(1, 2, 18)
+	graphEdgeArray.AddEdgeInGraphEA(1, 6, 16)
+	graphEdgeArray.AddEdgeInGraphEA(1, 8, 12)
+	graphEdgeArray.AddEdgeInGraphEA(2, 3, 22)
+	graphEdgeArray.AddEdgeInGraphEA(2, 8, 8)
+	graphEdgeArray.AddEdgeInGraphEA(3, 4, 20)
+	graphEdgeArray.AddEdgeInGraphEA(3, 4, 17)
+	graphEdgeArray.AddEdgeInGraphEA(3, 8, 21)
+	graphEdgeArray.AddEdgeInGraphEA(3, 7, 16)
+	graphEdgeArray.AddEdgeInGraphEA(4, 5, 26)
+	graphEdgeArray.AddEdgeInGraphEA(4, 7, 7)
+	graphEdgeArray.AddEdgeInGraphEA(5, 6, 17)
+	graphEdgeArray.AddEdgeInGraphEA(6, 7, 19)
+	t.Log(printGEA(graphEdgeArray))
+	graph := graphEdgeArray.MiniSpanTree_Kruskal()
+	t.Log(printGEA(graph))
 }
