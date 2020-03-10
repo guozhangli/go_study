8d27e7e835ab05642a6f3325e2d797a91e6de630
ligz <ligzh@irisking.com>
2020-03-10 16:12:52 +0800

    add fabonacci search

--- datastructure/Search.go
+++ datastructure/Search.go
@@ -35,3 +35,45 @@ func Interpolation_Search(arr []int, key int) int {
 	}
 	return -1
 }
+
+//斐波那契查找
+func Fabonacci_Search(arr []int, key int) int {
+	size := len(arr)
+	fab := BuildFabArr(size - 1)
+	fibBlock := len(fab) - 1
+	left := 0
+	right := size - 1
+	var mid int
+	for left <= right {
+		mid = left + fab[fibBlock-1] - 1
+		if key < arr[mid] {
+			right = mid - 1
+			fibBlock--
+		} else if key > arr[mid] {
+			left = mid + 1
+			fibBlock -= 2
+		} else {
+			if mid >= size {
+				return size - 1
+			} else {
+				return mid
+			}
+		}
+	}
+	return -1
+}
+
+func BuildFabArr(val int) []int {
+	fab := make([]int, 0)
+	fab = append(fab, 1)
+	fab = append(fab, 1)
+	i := 1
+	for {
+		fab = append(fab, fab[i]+fab[i-1])
+		i++
+		if fab[i] > val {
+			break
+		}
+	}
+	return fab
+}
--- datastructure/Search_test.go
+++ datastructure/Search_test.go
@@ -18,6 +18,13 @@ func TestInterpolation_Search(t *testing.T) {
 	for _, v := range arr {
 		index := Interpolation_Search(arr, v)
 		fmt.Println(index)
+	}
+}
 
+func TestFabonacci_Search(t *testing.T) {
+	arr := []int{0, 1, 16, 24, 35, 47, 59, 62, 73, 88, 99}
+	for _, v := range arr {
+		index := Fabonacci_Search(arr, v)
+		fmt.Println(index)
 	}
 }
