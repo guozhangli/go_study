8a3f9de1fa077ee4f9ef6ea005613e6d7b9c83e0
ligz <ligzh@irisking.com>
2020-03-18 18:53:45 +0800

    add sort function

--- datastructure/Sorted.go
+++ datastructure/Sorted.go
@@ -2,12 +2,71 @@ package TestProject
 
 //冒泡排序
 func BubblingSort(arr []int) []int {
-	for i := 0; i < len(arr); i++ {
-		for j := i; j < len(arr); j++ {
+	var flag = true
+	for i := 0; (i < len(arr)) && flag; i++ {
+		flag = false
+		for j := i + 1; j < len(arr); j++ {
 			if arr[j] < arr[i] {
 				arr[j], arr[i] = arr[i], arr[j]
+				flag = true
 			}
 		}
 	}
+	return arr
+}
+
+//简单选择排序
+func SimpleSearchSort(arr []int) []int {
+	var min int
+	for i := 0; i < len(arr); i++ {
+		min = i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[min] > arr[j] {
+				min = j
+			}
+		}
+		if i != min {
+			arr[i], arr[min] = arr[min], arr[i]
+		}
+	}
+	return arr
+}
+
+//插入排序
+func InsertSort(arr []int) []int {
+	var temp int
+	for i := 1; i < len(arr); i++ {
+		if arr[i] < arr[i-1] {
+			temp = arr[i]
+			var j int
+			for j = i - 1; j >= 0 && arr[j] > temp; j-- {
+				arr[j+1] = arr[j]
+			}
+			arr[j+1] = temp
+		}
+	}
+	return arr
+}
+
+//希尔排序
+func ShellSort(arr []int) []int {
+	for gap := len(arr) / 2; gap > 0; gap /= 2 {
+		for i := gap; i < len(arr); i++ {
+			InsertI(arr, gap, i)
+		}
+	}
+	return arr
+}
+func InsertI(arr []int, gap int, i int) {
+	var temp = arr[i]
+	var j int
+	for j = i - gap; j >= 0 && arr[j] > temp; j -= gap {
+		arr[j+gap] = arr[j]
+	}
+	arr[j+gap] = temp
+}
+
+//堆排序
+func HeapSort(arr []int) []int {
 	return arr
 }
--- datastructure/Sorted_test.go
+++ datastructure/Sorted_test.go
@@ -2,8 +2,32 @@ package TestProject
 
 import "testing"
 
-func TestSortedBubbling(t *testing.T) {
+func TestBubblingSorted(t *testing.T) {
 	var arr = []int{2, 4, 5, 9, 3, 6, 7, 1, 10, 8}
 	arrsorted := BubblingSort(arr)
+	t.Log(arrsorted)
+}
+
+func TestSimpleSearchSorted(t *testing.T) {
+	var arr = []int{2, 4, 5, 9, 3, 6, 7, 1, 10, 8}
+	arrsorted := SimpleSearchSort(arr)
+	t.Log(arrsorted)
+}
+
+func TestInsertSorted(t *testing.T) {
+	var arr = []int{2, 4, 5, 9, 3, 6, 7, 1, 10, 8}
+	arrsorted := InsertSort(arr)
+	t.Log(arrsorted)
+}
+
+func TestShellSorted(t *testing.T) {
+	var arr = []int{2, 4, 5, 9, 3, 6, 7, 1, 10, 8}
+	arrsorted := ShellSort(arr)
+	t.Log(arrsorted)
+}
+
+func TestHeapSorted(t *testing.T) {
+	var arr = []int{2, 4, 5, 9, 3, 6, 7, 1, 10, 8}
+	arrsorted := HeapSort(arr)
 	t.Log(arrsorted)
 }
