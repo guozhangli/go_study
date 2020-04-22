17f1bf11a5bb631ce24aa1ecb6bc89f7b91f7f21
ligz <ligzh@irisking.com>
2020-04-22 18:51:48 +0800

    go 协程

---
+++ awesomeProject1/src/main/goruntine/goruntine.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"datastructure"
+)
+// 定义任务类型
+type Task struct {
+	f func() error
+}
+var queue =TestProject.NewQueueLinked()
+
+func Take(arg_f func() error){
+	task:=&Task{
+		f:arg_f,
+	}
+	queue.EnQueue(task)
+}
+
+func (task *Task) Execute(){
+	task.f()
+}
+
+
