95647df091c5831f3554c7253bdabb9a4f738527
ligz <ligzh@irisking.com>
2021-02-23 18:16:14 +0800

    添加注释

--- Concurrent/keyword/TdIdf_test.go
+++ Concurrent/keyword/TdIdf_test.go
@@ -8,5 +8,8 @@ func TestTdIdf(t *testing.T) {
 	var doc3 = []string{"互联网", "的", "未来", "在", "人工", "智能"}
 	var doc4 = []string{"谷歌", "开源", "机器", "学习", "工具"}
 	var docs = [][]string{doc1, doc2, doc3, doc4}
-	t.Log(tdidf(doc2, docs, "谷歌"))
+	t.Log(td(doc2, "谷歌"))          //词频
+	t.Log(df(docs, "谷歌"))          //文档频率
+	t.Log(idf(docs, "谷歌"))         //逆文档频率
+	t.Log(tdidf(doc2, docs, "谷歌")) //权重
 }
